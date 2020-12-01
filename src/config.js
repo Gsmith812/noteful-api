9cf0b9c2aa08e58d61abb2da0da9d70903cd6788
George Smith <georgesmith812@gmail.com>
2020-12-01 00:39:31 -0500

    prepared file for heroku deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgressql://dunder_mifflin:dmiff@localhost/noteful',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgressql://dunder_mifflin:dmiff@localhost/noteful',
 }
